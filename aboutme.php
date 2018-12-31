d7d5fd410d7b2ac6e3443e4dc5ccc23f632a4fc8
Francisco <36778490+Grizzly95@users.noreply.github.com>
2018-12-30 19:27:37 -0600

    inicio web
    v1

---
+++ aboutme.php
@@ -0,0 +1,98 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Mas sobre mi</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="inner">
+
+							<!-- Logo -->
+								<a href="game/game.html" class="logo">
+									<span class="symbol"></span><span class="title"><i class="fas fa-user-ninja">Francisco</i></span>
+								</a>
+
+							<!-- Nav -->
+								<nav>
+									<ul>
+										<li><a href="#menu">Menu</a></li>
+									</ul>
+								</nav>
+
+						</div>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<h2>Menu</h2>
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li><a href="habilidades.php">skills</a></li>
+							<li><a href="hobbies.php">Hobbies</a></li>
+							<li><a href="aboutme.php">about me</a></li>
+						</ul>
+					</nav>
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner" id="text">
+							<h2>un poco mas de mi</h2>
+							<span class="image main"><img src="images/pic13.jpg" alt="" /></span>
+							<p>
+							<h6> <i class="fas fa-feather-alt fa-2x"></i>Conocer constantemente nuevas personas</h6></p>
+							<p><h6> <i class="far fa-smile-wink fa-2x"></i>liderar lo que estè a mi alcanze para asegurarme que todo vaya bien</h6></p>
+							
+							<p><h6><i class="far fa-sticky-note fa-2x"></i>Atencion a los detalles </h6></p>
+							<p><h6><i class="fas fa-grin-squint fa-2x" ></i>Me gusta el humor, la ironia sobre todo cuando incomoda xd</h6></p>
+                            <p><h6><i class="fas fa-angry fa-2x" ></i>tengo una pequeña cicatriz en una ceja, la hizo mi hermano con un embase cuando estabamos pequeños  </h6></p>
+                            <p><h6><i class="fas fa-heartbeat fa-2x" ></i>Sentir pasión es mi principal fuente de inspiración en la vida.</h6></p>
+                            <p><h6><i class="fab fa-angellist fa-2x"></i>Me resulta “curioso” que gente que no conozca sepa  cosas sobre mí... </h6></p>
+                              
+						
+						</div>
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<div class="inner">
+							<section>
+							<h2><p>sigueme-</p></h2>
+								<br></br>
+								<ul class="icons">
+									<li><a href="#" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="https://www.facebook.com/franciscogrizaguilar" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
+									<li><a href="#" class="icon style2 fa-dribbble"><span class="label">Dribbble</span></a></li>
+									<li><a href="#" class="icon style2 fa-github"><span class="label">GitHub</span></a></li>
+									<li><a href="#" class="icon style2 fa-500px"><span class="label">500px</span></a></li>
+									<li><a href="#" class="icon style2 fa-phone"><span class="label">Phone</span></a></li>
+									<li><a href="#" class="icon style2 fa-envelope-o"><span class="label">Email</span></a></li>
+								</ul>
+							</section>
+							<ul class="copyright">
+								<li>&copy; Francisco. todos los derechos reservados</li>
+							</ul>
+						</div>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ hobbies.php
@@ -0,0 +1,99 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Hobbiess</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="inner">
+
+							<!-- Logo -->
+								<a href="game/game.html" class="logo">
+									<span class="symbol"></span><span class="title"><i class="fas fa-user-ninja">Francisco</i></span>
+								</a>
+
+							<!-- Nav -->
+								<nav>
+									<ul>
+										<li><a href="#menu">Menu</a></li>
+									</ul>
+								</nav>
+
+						</div>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<h2>Menu</h2>
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li><a href="habilidades.php">Skills</a></li>
+							<li><a href="hobbies.php">Hobbies</a></li>
+							
+							<li><a href="aboutme.php">About me</a></li>
+						</ul>
+					</nav>
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+							<h1>Hobbies</h1>
+							<span class="image main"><img src="images/pic13.jpg" alt="" /></span>
+							<p>
+							<h3><i class="fas fa-headset fa-2x"></i>Producciòn de audios </h3></p>
+							<p><h3><i class="fas fa-microphone-alt fa-2x"></i>rapero, escritor (rap conciencia)</h3></p>
+						
+						<p><h5><i class="fab fa-youtube fa-2x"></i>en un rincon de YT</h5></p>
+						
+							
+							<p><h3><i class="fas fa-play fa-2x"></i>Escuchar musica 24/7 casi sin exagerar..</h3></p>
+
+
+							<p><h3><i class="fas fa-coffee fa-2x"></i>haaa y tomar màs cafè  ...</h3></p>
+						</div>
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<div class="inner">
+							<section>
+							<h2><p>sigueme-</p></h2>
+								<br></br>
+								<ul class="icons">
+									<li><a href="#" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="https://www.facebook.com/franciscogrizaguilar" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
+									<li><a href="#" class="icon style2 fa-dribbble"><span class="label">Dribbble</span></a></li>
+									<li><a href="#" class="icon style2 fa-github"><span class="label">GitHub</span></a></li>
+									<li><a href="#" class="icon style2 fa-500px"><span class="label">500px</span></a></li>
+									<li><a href="#" class="icon style2 fa-phone"><span class="label">Phone</span></a></li>
+									<li><a href="#" class="icon style2 fa-envelope-o"><span class="label">Email</span></a></li>
+								</ul>
+							</section>
+							<ul class="copyright">
+								<li>&copy; Francisco. todos los derechos reservados</li>
+							</ul>
+						</div>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
